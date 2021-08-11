11a2e4b1f57f4f92dc75ac7b0602df4a4ee0e071
roger chat <mattrogerpelosi@gmail.com>
2021-08-11 13:51:37 -0400

    commit tree 57e662170d6dda1ebe62f368c7e3a02b7bed96dc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function superbowl(array) {
+    for(const key of array) {
+        if(key.result === "W") {
+          return key.year;   
+    }
+  } 
+}
 
+function superbowlWin(array) {
+    const result = array.find(array => array.result === "W");
+     if(result !== undefined) {
+         return result.year;
+     } else {
+         return undefined;
+     }
+}
